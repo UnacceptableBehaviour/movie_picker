03592a81c226d410713012b6410d0fdf64d0ad88
Simon <simon@ferdiesfoodlab.co.uk>
2020-06-18 20:22:10 +0100

    add iterate by sorted - year, rating, title

--- iterator_example.py
+++ iterator_example.py
@@ -13,6 +13,7 @@ from collections.abc import Iterable, Iterator
 
 
 import sys		# sys.exit()
+from pprint import pprint
 
 FORWARD = 1
 REVERSE = -1
@@ -48,23 +49,46 @@ class Movie:
 	def __str__(self):
 		return f"{self.year} {self.rating.rjust(4)} - {self.title}"
 
+	def __repr__(self):
+		return f"{self.year} {self.rating.rjust(4)} - {self.title}"
+	
+	def __lt__(self):	# support bisect or http://code.activestate.com/recipes/577197-sortedcollection/
+		pass			# in dir ./scripts 
+
 
 class MediaLib(Iterable):
 	def __init__(self, media_list=[]):
 		self.media_files = media_list
+		self.sort_lists()
 
-	def __iter__(self) -> MediaLibIter:		
+	def add_media(self, media):		
+		self.media_files.append(media)
+		# http://code.activestate.com/recipes/577197-sortedcollection/
+		self.sort_lists() 	# TODO maintain a sorted collection ^
+		
+	def sort_lists(self):
+		self._sorted_by_year = sorted(self.media_files, key=lambda x: x.year)	# in place media_files.sort(key=lambda x: x.year)
+		self._sorted_by_title = sorted(self.media_files, key=lambda x: x.title)
+		self._sorted_by_rating = sorted(self.media_files, key=lambda x: float(x.rating), reverse=True)
+
+	def __iter__(self) -> MediaLibIter:									#  -> MediaLibIter is optional guide to coder & toolchain
 		return MediaLibIter(self.media_files)
 	
-	def reverse_each(self):
+	def reverse_each(self) -> MediaLibIter:
 		return MediaLibIter(self.media_files, direction=REVERSE)
 	
+	def sorted_by_year(self, direction=FORWARD) -> MediaLibIter:
+		#direction = REVERSE if direction==REVERSE else FORWARD			# check done in iterato redundant
+		return MediaLibIter(self._sorted_by_year, direction)	
 	
-
+	def sorted_by_title(self, direction=FORWARD) -> MediaLibIter:
+		return MediaLibIter(self._sorted_by_title, direction)	
 	
-
-
+	def sorted_by_rating(self, direction=FORWARD) -> MediaLibIter:
+		return MediaLibIter(self._sorted_by_rating, direction)
 		
+	
+
 
 def main():
 	
@@ -81,11 +105,22 @@ def main():
 	for media in media_library:
 		print(media)
 	
+	media_library.add_media(Movie("The Rattlers Cage",'2020','10.0'))
+	
 	print(". . and for my next trick . . . BACKWARDS . . . ooh!")
 	
 	for media in media_library.reverse_each():
 		print(media)
 
+	print(". . and for my next trick . . . SORTED BY YEAR . . . aaah!")
+	
+	for media in media_library.sorted_by_year():
+		print(media)
+
+	print(". . and for my next trick . . . SORTED YEAR BACKWARDS . . . waah!?")
+	
+	for media in media_library.sorted_by_year(REVERSE):
+		print(media)
 
 if __name__ == '__main__':
 
