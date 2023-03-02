f50908fd2938592ba41dd8c3689b3bac63bea5cf
UnacceptableBehaviour <fernandez.simon@gmail.com>
2023-03-02 15:30:02 +0000

    update dl_session.json DLOAD_SESSION_DB after completion

--- scripts/wtdl.py
+++ scripts/wtdl.py
@@ -28,10 +28,14 @@ import sys
 from pathlib import Path
 import json
 import re
+import atexit
 from pprint import pprint
 import threading
 import time
 from datetime import datetime       # datetime.now().timestamp() = 1666356102.098952
+
+import traceback
+
 
 # file IO helpers - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \
 #
@@ -57,16 +61,22 @@ def load_dict_data_from_DB(cDB, db_path):
         commit_dict_to_DB(db, db_path)
         return -1
 
+
 def commit_dict_to_DB(commit_db, db_path):
     '''
     commit dict info to text file - json format
     '''
-
+    # TODO create dir if it doesn't exist
+    print(f"COMMIT: {db_path}", end='')
+    
     with open(db_path, 'w') as f:
         #pprint(commit_db)
         db_as_json = json.dumps(commit_db)
         f.write(db_as_json)
-        
+
+    print(' - DONE:')
+
+
 def get_urls_from_file(filename):
     with open(filename, 'r') as f:
         content = f.read()
@@ -80,6 +90,21 @@ def get_urls_from_file(filename):
         url_list.append(line)   # maybe add regex to check valid url
 
     return url_list
+
+@atexit.register                # smae as atexit.register(write_state_to_disk)
+def write_state_to_disk():
+    download_thread_info_remaining = {}
+    for channel,dloads in download_thread_info_dict.items():
+        if len(dloads) > 0: download_thread_info_remaining[channel] = download_thread_info_dict[channel]
+    
+    if len(download_thread_info_remaining) > 0: 
+        commit_dict_to_DB(download_thread_info_remaining, Dload.DLOAD_SESSION_DB)
+    else:
+        print('DOWNLOADS COMPLETED - NO ERRORS')
+        Path.unlink(Dload.DLOAD_SESSION_DB)
+
+    commit_dict_to_DB(channel_DB, CHANNEL_DB_FILE)
+
 #
 # file IO helpers - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - /
 
@@ -105,8 +130,10 @@ def get_playlist_update(cDB, chan_key, group_dir, pl_url, ydl_opts_pass={}):
     if chan_key not in cDB.keys(): ydl_opts['playlist_items'] = None   # download everything
     pprint(ydl_opts)
 
+    # download play list whole if new or in part if updating
     ydl = youtube_dl.YoutubeDL(ydl_opts)
 
+    # gather relevant info about playlist items
     with ydl:
         result = ydl.extract_info(pl_url, download=False) 
     
@@ -118,9 +145,10 @@ def get_playlist_update(cDB, chan_key, group_dir, pl_url, ydl_opts_pass={}):
                 try:
                     if video == None:
                         print(f"{(i):03}: K: -- -- - * * * * Download not available * * * *")
+                        pprint(video)
                         continue
                     else:
-                        print(f"{(i):03}: K:{video['webpage_url_basename']} {video['webpage_url']} - {video['title']}")   # {i:03} left pad n with 0's 3 digits
+                        print(f"P:{(i):04} I:{(video['playlist_index']):04} K:{video['webpage_url_basename']} {video['webpage_url']} - {video['title']}")   # {i:03} left pad n with 0's 3 digits
                         play_list[video['webpage_url_basename']] = { 'src_url': video['webpage_url'],
                                                                      'group_dir': '',
                                                                      'target_dir': chan_key,                                                                     
@@ -133,13 +161,20 @@ def get_playlist_update(cDB, chan_key, group_dir, pl_url, ydl_opts_pass={}):
                     print(f"> get_playlist - Error <{i}>")
                     pprint(e)
 
+    # return entries not in the download DB
     if chan_key in cDB.keys():
+        print(f"Finding new entries in > {chan_key} <")
         vid_pos = None
+        
         for k, video in play_list.items():
+            print('- - - - - - - - - - - - - \ ')
+            pprint(video)
+        
             if k in cDB[chan_key].keys():
                 vid_pos = cDB[chan_key][k]['pos']
                 print(f"P:{(video['pos']):04} K:{k} {video['src_url']} - {video['title']}")
                       # P-resent video
+            
             if k not in cDB[chan_key].keys():
                 new_vid_entries[k] = video
                 vid_pos += 1
@@ -205,8 +240,8 @@ class DloadProgressDisplay(threading.Thread):
         
     def run(self):
         print("STARTED . . . DloadProgressDisplay . . waiting for downloads to start")
-                
-        while (Dload.registrations == 0):   # WAITING_FOR_DOWLOANDS_TO_START
+        
+        while (len(Dload.prog_dict) == 0) and (Dload.dloads_complete < 1):   # WAITING_FOR_DOWLOANDS_TO_START
             time.sleep(1)
         
         time.sleep(1) # (Allow Dload.all_done to change to False)
@@ -217,18 +252,35 @@ class DloadProgressDisplay(threading.Thread):
 
         print("ALL DONE . . .")
         sys.exit(0)
-    
+
+
+
+# TODO - move into class?
+download_thread_info_dict = {}      # Stored DLOAD_SESSION_DB
 
 class Dload(threading.Thread):
-    tick = 0                      # # #
-    prog_dict = {}                    #
-    registrations = 0                 #
-    all_done = False                  #
-    total_bandwidth_use = 0           #
-    prog_lock = threading.Lock()  # # #
+    tick = 0                                # # #
+    prog_dict = {}                              # use for registrations
+    dloads_complete = 0                         #
+    all_done = False                            #
+    total_bandwidth_use = 0                     #
+    DLOAD_SESSION_DB = Path('./chanDloads/dl_session.json')  # ALL files to be downloaded in this session
+    dload_session_db_lock = threading.Lock()    #
+    prog_lock = threading.Lock()            # # #
     
     @staticmethod
-    def prog_hook(dl_data):        
+    def prog_hook(dl_data):
+        # dl_data - - - - - - - - - - \     # TODO - REMOVE
+        # {'_elapsed_str': '01:16:58',
+        # '_total_bytes_str': '203.44MiB',
+        # 'downloaded_bytes': 213327478,
+        # 'elapsed': 4618.32977104187,
+        # 'filename': 'chanDloads/chan/@LivBoeree/14-The Future of Humanity _ Anders '
+        #             'Sandberg and Liv Boeree-crFk-fDBu6A.mp4',
+        # 'status': 'finished',
+        # 'total_bytes': 213327478}
+        # dl_data - - - - - - - - - - /     
+        #     
         #print("\x1B\x5B1J") # clear screen above the cursor ESC [ 1 J - https://en.wikipedia.org/wiki/ANSI_escape_code
                             # https://ss64.com/ascii.html
         if '_percent_str' in dl_data:
@@ -243,19 +295,75 @@ class Dload(threading.Thread):
         eta = 9999
         try:
             eta = f"{int(dl_data['eta'] / 60)}m{int(dl_data['eta'] % 60)}".rjust(6, ' ')
-        except Exception:
+        except Exception as err: # TODO - specify Exception
+            print(f"# # > ETA except:{err} < <        -        -        - <<")
             if dl_data['status'] == 'finished':
                 with Dload.prog_lock:
+                    print('dl_data - - - - - - - - - - \ ') # TODO remove
+                    pprint(dl_data)
+                    print('dl_data - - - - - - - - - - / ')
                     if dl_data['filename'] in Dload.prog_dict:
-                        del Dload.prog_dict[dl_data['filename']]                
+                        del Dload.prog_dict[dl_data['filename']]
+                    Dload.dloads_complete += 1
+                
+                with Dload.dload_session_db_lock: # mark as downloaded
+                    chan_or_file_dir = chan_or_file_name = title = url_basename = None
+                    m = re.search(r'chanDloads\/(.*?)\/(.*?)\/\d+-(.*?)-([a-zA-Z0-9\-_]{11})\.', dl_data['filename'])
+                    if m:
+                        chan_or_file_dir  = m.group(1)
+                        chan_or_file_name = m.group(2)
+                        title             = m.group(3)
+                        url_basename      = m.group(4)
+                    # move out of download_thread_info_dict
+                    # add filename
+                    # mark as downloaded
+                    # move into channel_DB, 
+                    print(f"\nurl_basename:{url_basename} - {chan_or_file_dir}/{chan_or_file_name} - {title}")
+                    for dlt_chan_name,dlt_list in download_thread_info_dict.items():
+                        if dlt_chan_name == chan_or_file_name:
+                            for i, dlt in enumerate(dlt_list):
+                                if url_basename in dlt['src_url']:
+                                    print(f"FOUND: {url_basename} @ {i}")
+                                    dlt['downloaded'] = True
+                                    dlt['url_basename'] = url_basename
+                                    if dlt_chan_name in channel_DB.keys():
+                                        channel_DB[dlt_chan_name][url_basename] = dlt
+                                    else:
+                                        channel_DB[dlt_chan_name] = { url_basename: dlt }
+                                    print('dlt - - - - - - - - - - \ ')
+                                    pprint(dlt)
+                                    print('dlt - - - - - - - - - - / ')
+                                    del(dlt_list[i]) # dlt
+                                    print(f"-*- DEL:{i}")
+                                    commit_dict_to_DB(download_thread_info_dict, Dload.DLOAD_SESSION_DB)
+                                    break
+
+
+
                 print(f"FINISHED: {dl_data['filename']}")
                 return
 
         with Dload.prog_lock:            
             Dload.prog_dict[dl_data['filename']] = { 'p_bar': f"|{progress}|{pc_str} {dl_data['_total_bytes_str'].rjust(10, ' ')} {eta} {title} {dl_data['status']}",
                                                      'stats': dl_data }
-            if Dload.registrations < len(Dload.prog_dict): Dload.registrations = len(Dload.prog_dict)
-        
+
+
+# Update this at the end of each thread
+# Semtake on fileLock:
+# with Dload.dload_session_db_lock:
+#   update download_thread_info_dict    (saved later) commit_dict_to_DB(download_thread_info_dict, Dload.DLOAD_SESSION_DB)
+#       remove the download. 
+#       -([a-zA-Z0-9\-_]{11})\.    
+# 
+# Add to channel_DB as downloaded
+#   Commit that to file
+#   
+#     
+# dictof 
+#     key = target_dir
+#         LIST
+#         src_url
+#         target_dir
 
     @staticmethod
     def combine_stats():
@@ -319,6 +427,7 @@ class Dload(threading.Thread):
         self.ydl_opts.update(ydl_opts)
         self.ydl = youtube_dl.YoutubeDL(self.ydl_opts)
     
+
     def run(self):
         # TODO wait on bandwidth available
         self.ydl.download([self.url_to_fetch])
@@ -334,7 +443,9 @@ def create_dld_thread_info( details={} ):
                     'idx': None,
                     'pos': None,
                     'src_url': '',
-                    'title': '' }
+                    'title': '',
+                    'url_basename': '' }
+    
     thread_info.update(details)
     #pprint(thread_info)
     return thread_info
@@ -342,8 +453,8 @@ def create_dld_thread_info( details={} ):
 def print_download_intent(download_thread_info_dict):
     #pprint(download_thread_info_dict)
     for chan, info in download_thread_info_dict.items():
-        print(f"\n# # # # #   Chan: {chan} ({len(info)})  - Dir: base/group/chan")
-        pprint(info[0])
+        print(f"\n# # # # #   Chan: {chan} ({len(info)})  - Dir: base/group/chan")        
+        if len(info) > 0: pprint(info[0])
     
 
 
@@ -352,15 +463,8 @@ def print_download_intent(download_thread_info_dict):
 # ENTRY POINT __main__
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-
-# ALL files to be downloaded in this session
-DLOAD_SESSION_DB = Path('./chanDloads/dl_session.json')
-download_thread_info_dict = {}
-
-if DLOAD_SESSION_DB.exists():
-    load_dict_data_from_DB(download_thread_info_dict, DLOAD_SESSION_DB)
-    print("* * * * Previous download session exists CONTINUE downloads? * * * *")
-    # TODO implement persistence flow
+if Dload.DLOAD_SESSION_DB.exists():
+    load_dict_data_from_DB(download_thread_info_dict, Dload.DLOAD_SESSION_DB)
     
 file_dload_only = False
 file_channel = None
@@ -396,7 +500,8 @@ if ('-f' in sys.argv) or ('-fo' in sys.argv):
         del(sys.argv[option_f_index+1])
         del(sys.argv[option_f_index])
     else:
-        print(f"* * * WARNING * * *\nFile spcified by option -f/fo\nNOT FOUND:{dload_file} <")
+        print(f"\n\n* * * WARNING * * *\nFile spcified by option -f/fo\nNOT FOUND:{dload_file} <\n\n")
+        sys.exit(0)
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -411,7 +516,7 @@ download_targets_all = {}
 if '-r' in sys.argv:        # - - - - - - - - - - - - - - - - - - - - - - - - RELOAD (-r) & RECORD NEW (-u)
     for channel_url in video_channel_urls:
         print(f"URL: {channel_url}")
-        channel_key = channel_url.replace('https://www.youtube.com/c/','').replace('https://www.youtube.com/user/','').replace('https://www.youtube.com/channel/','').replace('/videos','') 
+        channel_key = channel_url.replace('https://www.youtube.com/','').replace('/videos','') 
         print(f"channel_key: {channel_key}")
         video_dict = None
         recent_chan_vid_info = None
@@ -423,8 +528,11 @@ if '-r' in sys.argv:        # - - - - - - - - - - - - - - - - - - - - - - - - RE
                 recent_chan_vid_info = get_playlist_update(channel_DB, channel_key, group_dir, channel_url, {'playlist_items': playlist_items, 'verbose':True, 'forceurl':True})
         except BaseException as e:
             print('> - Error getting PLay List Info')
-            pprint(e)            
-        finally:            
+            print(traceback.format_exc())
+            print('-')
+            pprint(e)
+            print('==')
+        finally:
             if ('-u' in sys.argv) and (video_dict or recent_chan_vid_info):
                 print('> - SAVING DATA')
                 if channel_key in channel_DB:
@@ -468,12 +576,9 @@ if len(download_thread_info_dict) > 0:
     yn = input('Continue (y)/n\n')
     if yn.strip().lower() == 'n': sys.exit(0)
 
-# TODO add before exit hook to ensure persistence across exec runs
-# AFTER ALL OPTIONS PROCESSED SAVE DLOAD SESSION INFO as JSON        
-commit_dict_to_DB(download_thread_info_dict, DLOAD_SESSION_DB)
-# sys.exit(0)
 
-#sys.exit(0)
+# save the intent for the download session
+commit_dict_to_DB(download_thread_info_dict, Dload.DLOAD_SESSION_DB)
 
 thread_list = []
 for target_dir, vid_list in download_thread_info_dict.items():
@@ -490,17 +595,23 @@ for t in thread_list:
 
 if '-i' in sys.argv:
     print(f"\n> Dload Info [{len(download_thread_info_dict)}]: - - - - - - - - - - - <")
-    for chan,vid_dict in download_thread_info_dict:
-        print(f"C: {chan} - {len(vid_dict)}") 
+    if len(download_thread_info_dict):
+        pprint(next(iter(download_thread_info_dict)))   # dump an item
+        pprint(download_thread_info_dict[next(iter(download_thread_info_dict))])
+        for chan,vid_list in download_thread_info_dict.items():
+            print(f"C: {chan} - {len(vid_list)}") 
         
     print(f"\nDload dict download_thread_info_dict size: {len(download_thread_info_dict)}")
     print(f"Thread list size: {len(thread_list)}")
+    print(f"\n> Channel Subscriptions [{len(video_channel_urls)}]: - - - - - - - - - - - <")
 
 if len(thread_list) > 0:
     DloadProgressDisplay().start()
 else:
     print('\nNothing scheduled for download\nUse -h for help')
 
+
+# atexit: write_state_to_disk()
 sys.exit(0) # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -508,7 +619,6 @@ sys.exit(0) # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 # TODO
 # H - - - - - - - 
-# -f / -fo doesn't cleanup / update dl_session.json DLOAD_SESSION_DB
 # Add help with option info & examples
 # Add -i option: DB info dump
 # assess / sort channel function items
@@ -529,7 +639,7 @@ sys.exit(0) # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #
 # channel function
 # when checking playlists for new content check 10,20,40,80,160,320 until caught up 
-# update DLOAD_SESSION_DB at end - or DELETE or rename for history / end of week compiliation
+# update Dload.DLOAD_SESSION_DB at end - or DELETE or rename for history / end of week compiliation
 # store this weeks new content in one folder for transfer to other devices
 # get X back cataloque downloads per week - add setting
 
